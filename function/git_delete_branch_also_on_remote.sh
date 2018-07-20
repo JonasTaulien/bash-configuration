5df7e3a41d195de072b846f875051b4d0c125c09
Jonas Rudolph <j.rudolph@tallence.com>
2018-07-20 14:11:22 +0200

    Added possibility to specify remote-branch-name in jr_funct_git_delete_branch_also_on_remote

--- function/git_delete_branch_also_on_remote.sh
+++ function/git_delete_branch_also_on_remote.sh
@@ -11,12 +11,14 @@ alias jr_doc_funct_git_delete_branch_also_on_remote="echo \"
 #
 # @param1 name of the branch to delete
 # @param2 name of the remote. Default: 'origin'
+# @param2 name of the branch on the remote. Default: @param1
 ##########\""
 jr_funct_git_delete_branch_also_on_remote(){
     local branchToDelete="${1}"
     local remoteName="${2:-origin}"
+    local remoteBranchName="${3:-${branchToDelete}}"
 
-    git push "${remoteName}" --delete "${branchToDelete}"
+    git push "${remoteName}" --delete "${remoteBranchName}"
     git branch -d "${branchToDelete}"
 }
 export -f jr_funct_git_delete_branch_also_on_remote
