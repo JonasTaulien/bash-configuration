9b1f0294d960b9431f8457d2da1e75f0bd21d28b
Jonas Rudolph <jonas.ru@gmx.de>
2018-09-23 21:31:17 +0200

    Added sensible bash and pdf_concat

--- shortcuts.sh
+++ shortcuts.sh
@@ -18,10 +18,9 @@ alias lsl='ls -plA'
 
 
 alias resource_bashrc='source ~/.bashrc'
-
+alias pdf_concat='jr_funct_pdf_concat'
 
 alias git_add_all_and_commit='git add -A && git commit -m'
-
 alias git_create_branch='jr_funct_git_create_branch'
 alias git_delete_branch_locally='jr_funct_git_delete_branch_locally'
 alias git_delete_branch_on_remote='jr_funct_git_delete_branch_on_remote'
@@ -30,3 +29,6 @@ alias git_rename_branch_also_on_remote='jr_funct_git_rename_branch_also_on_remot
 alias git_rename_current_branch_also_on_remote='jr_funct_git_rename_current_branch_also_on_remote'
 alias git_delete_tag_also_on_remote='jr_funct_git_delete_tag_also_on_remote'
 alias git_undo_last_commit='jr_funct_git_undo_last_commit'
+
+# Directories (Just type `$var<tab>` to cd into it)
+export imp="$HOME/Important"
