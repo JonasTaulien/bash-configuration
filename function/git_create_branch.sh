dbf8dba85005f027472978ba854e66c241e3f63c
Jonas Rudolph <j.rudolph@tallence.com>
2018-09-24 12:52:54 +0200

    Added stiing of remote branch name in git_create_branch

--- function/git_create_branch.sh
+++ function/git_create_branch.sh
@@ -11,12 +11,14 @@ alias jr_doc_funct_git_create_branch="echo \"
 #
 # @param1 name of the branch
 # @param2 name of the remote. Default: 'origin'
+# @param2 name of the branch on the remote. Default: @param1
 ##########\""
 jr_funct_git_create_branch(){
     local branchToCreate="${1}"
     local remoteName="${2:-origin}"
+    local remoteBranchName="${3:-${branchToCreate}}"
 
     git checkout -b "${branchToCreate}"
-    git push --no-verify --set-upstream "${remoteName}" "${branchToCreate}"
+    git push --no-verify --set-upstream "${remoteName}" "${remoteBranchName}"
 }
 export -f jr_funct_git_create_branch
